3cb00f9c84d0ca02674b229ef73f75cac9c90abf
timi <tomdvazq@gmail.com>
2022-05-25 23:56:28 -0300

    changes

---
+++ js/code.js
@@ -0,0 +1,11 @@
+//Acordeón//
+
+let collapsibles = document.querySelectorAll('.aboutMe-data_container')
+
+collapsibles.forEach(element => {
+    element.addEventListener("click", () =>{
+        element.querySelector('.aboutMe-answer').classList.toggle('open');
+        element.querySelector('.aboutMe-sentence').classList.toggle('active');
+        element.querySelector('.arrow-container').classList.toggle('up');
+    })
+});
