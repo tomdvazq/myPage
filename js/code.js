d3a8a6f7a94ccc49e0070f0e91928042c5a111fe
timi <tomdvazq@gmail.com>
2022-05-27 18:01:12 -0300

    fix changes

--- js/code.js
+++ js/code.js
@@ -15,3 +15,38 @@ collapsibles.forEach(element => {
         element.querySelector('.arrow-container').classList.toggle('up');
     })
 });
+
+//Button//
+
+let readmore_btn = document.getElementById('readmore_btn');
+let hideText = document.getElementById('hideText');
+
+readmore_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show');
+    if(hideText.classList.contains('show')) {
+        readmore_btn.innerHTML = "Read Less";
+    }
+    else {
+        readmore_btn.innerHTML = "Read More";
+    }
+}
+
+//Read More Data//
+
+const li = document.querySelectorAll('.li')
+const block = document.querySelectorAll('.block')
+
+li.forEach((cadaLi, i)=>{
+    li[i].addEventListener('click',()=>{
+
+        li.forEach((cadaLi, i)=>{
+            li[i].classList.remove('active')
+            block[i].classList.remove('active')
+        })
+
+        li[i].classList.add('active')
+        block[i].classList.add('active')
+    })
+})
