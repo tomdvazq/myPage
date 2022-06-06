5c74462ebd6ba43da787797d94e530f10a71e9d2
timi <tomdvazq@gmail.com>
2022-06-06 13:29:12 -0300

    changes in php file

--- form.php
+++ form.php
@@ -9,13 +9,13 @@ $message = $_POST['message'];
 
 // Cuerpo del mail //
 
-$bodymail = "Este mensaje fue enviado por:" $nombre ",\r\n";
-$bodymail = "Perteneciente a la compañía:" $company ",\r\n";
-$bodymail = "Vive en:" $country ",\r\n";
-$bodymail = "Su e-mail es:" $mail ",\r\n";
-$bodymail = "Está buscando un tipo de trabajo:" $work ",\r\n";
-$bodymail = "Y adjuntó el mensaje:" $message ",\r\n";
-$bodymail = "Este e-mail fue enviado el" date('d/m/Y', time());
+$bodymail = "Este mensaje fue enviado por:" . $nombre . ",\r\n";
+$bodymail = "Perteneciente a la compañía:" . $company . ",\r\n";
+$bodymail = "Vive en:" . $country . ",\r\n";
+$bodymail = "Su e-mail es:" . $mail . ",\r\n";
+$bodymail = "Está buscando un tipo de trabajo:" . $work . ",\r\n";
+$bodymail = "Y adjuntó el mensaje:" . $message . ",\r\n";
+$bodymail = "Este e-mail fue enviado el" . date('d/m/Y', time());
 
 //Dirección//
 
