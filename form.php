57f839059c3ef6ddaa21acaac7167617dc60496c
timi <tomdvazq@gmail.com>
2022-06-06 13:47:48 -0300

    more changes in php file

--- form.php
+++ form.php
@@ -1,5 +1,4 @@
 <?php
-
 $nombre = $_POST['name'];
 $company = $_POST['company'];
 $country = $_POST['country'];
@@ -7,15 +6,18 @@ $mail = $_POST['email'];
 $work = $_POST['work'];
 $message = $_POST['message'];
 
+//Texto//
+$header .= "Content-Type: text/plain";
+
 // Cuerpo del mail //
 
-$bodymail = "Este mensaje fue enviado por:" . $nombre . ",\r\n";
-$bodymail = "Perteneciente a la compañía:" . $company . ",\r\n";
-$bodymail = "Vive en:" . $country . ",\r\n";
-$bodymail = "Su e-mail es:" . $mail . ",\r\n";
-$bodymail = "Está buscando un tipo de trabajo:" . $work . ",\r\n";
-$bodymail = "Y adjuntó el mensaje:" . $message . ",\r\n";
-$bodymail = "Este e-mail fue enviado el" . date('d/m/Y', time());
+$bodymail .= "Este mensaje fue enviado por:" . $nombre . ",\r\n";
+$bodymail .= "Perteneciente a la compañía:" . $company . ",\r\n";
+$bodymail .= "Vive en:" . $country . ",\r\n";
+$bodymail .= "Su e-mail es:" . $mail . ",\r\n";
+$bodymail .= "Está buscando un tipo de trabajo:" . $work . ",\r\n";
+$bodymail .= "Y adjuntó el mensaje:" . $message . ",\r\n";
+$bodymail .= "Este e-mail fue enviado el" . date('d/m/Y', time());
 
 //Dirección//
 
@@ -29,5 +31,4 @@ mail($for, $asunto, utf8_decode($bodymail), $header);
 //Redirección//
 
 header('Location:success.html');
-
 ?>
