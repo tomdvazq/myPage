30d62be4113a2fc2f10ea8b48cdb039b15636eff
timi <tomdvazq@gmail.com>
2022-06-07 12:31:02 -0300

    finish traduction

--- js/code.js
+++ js/code.js
@@ -51,3 +51,16 @@ li.forEach((cadaLi, i)=>{
     })
 })
 
+//Language//
+
+let check = document.getElementById('check');
+check.addEventListener('click', idioma);
+
+function idioma(){
+    let id = check.checked;
+    if (id==true){
+        location.href="es/index.html"
+    } else {
+        location.href="../index.html"
+    }
+}
