e5524fc4cf08e37a782a43531ca87a81270a45bf
timi <tomdvazq@gmail.com>
2022-06-07 11:33:00 -0300

    create spanish version

---
+++ es/js/codigo.js
@@ -0,0 +1,53 @@
+//Pre Loader//
+window.onload = function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+}
+
+//Acordeón//
+
+let collapsibles = document.querySelectorAll('.aboutMe-data_container')
+
+collapsibles.forEach(element => {
+    element.addEventListener("click", () =>{
+        element.querySelector('.aboutMe-answer').classList.toggle('open');
+        element.querySelector('.aboutMe-sentence').classList.toggle('active');
+        element.querySelector('.arrow-container').classList.toggle('up');
+    })
+});
+
+//Button//
+
+let readmore_btn = document.getElementById('readmore_btn');
+let hideText = document.getElementById('hideText');
+
+readmore_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show');
+    if(hideText.classList.contains('show')) {
+        readmore_btn.innerHTML = "Leer menos";
+    }
+    else {
+        readmore_btn.innerHTML = "Leer más";
+    }
+}
+
+//Read More Data//
+
+const li = document.querySelectorAll('.li')
+const block = document.querySelectorAll('.block')
+
+li.forEach((cadaLi, i)=>{
+    li[i].addEventListener('click',()=>{
+
+        li.forEach((cadaLi, i)=>{
+            li[i].classList.remove('active')
+            block[i].classList.remove('active')
+        })
+
+        li[i].classList.add('active')
+        block[i].classList.add('active')
+    })
+})
+
